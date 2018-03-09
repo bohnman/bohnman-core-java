2334de61a03f28c3ce4cdd2b463e6722a1a47dd1
Ryan Bohn <rbohn@biohitech.com>
2018-03-09 09:24:12 -0500

    adding io pacakges

---
+++ src/main/java/com/github/bohnman/core/encoding/CoreCharsets.java
@@ -0,0 +1,8 @@
+package com.github.bohnman.core.encoding;
+
+import java.nio.charset.Charset;
+
+public class CoreCharsets {
+
+    public static final Charset UTF_8 = Charset.forName("UTF-8");
+}
---
+++ src/main/java/com/github/bohnman/core/io/CoreIo.java
@@ -0,0 +1,27 @@
+package com.github.bohnman.core.io;
+
+import com.github.bohnman.core.encoding.CoreCharsets;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class CoreIo {
+
+    public static String toString(String path, Charset charset) {
+        try {
+            byte[] bytes = Files.readAllBytes(Paths.get(path));
+            return new String(bytes, charset);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    public static String toString(File file, Charset charset) {
+        return toString(file.getAbsolutePath(), charset);
+    }
+
+}
