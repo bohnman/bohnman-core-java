712aed7bee882439efaf6c761cb7e1d4fcbc0a45
Ryan Bohn <rbohn@biohitech.com>
2018-03-18 08:56:08 -0400

    Added safeToString which only converts basic types to string

--- src/main/java/com/github/bohnman/core/convert/CoreConversions.java
+++ src/main/java/com/github/bohnman/core/convert/CoreConversions.java
@@ -2,13 +2,32 @@ package com.github.bohnman.core.convert;
 
 import com.github.bohnman.core.collect.CoreArrays;
 import com.github.bohnman.core.function.CoreLambda;
+import com.github.bohnman.core.lang.CoreClasses;
+import com.github.bohnman.core.library.CoreLibraries;
+import org.joda.time.DateTime;
 
 import javax.annotation.Nullable;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
 import java.util.TimeZone;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class CoreConversions {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+    private static final DateTimeFormatter LOCAL_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
+    private static final DateTimeFormatter LOCAL_DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter LOCAL_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
 
     @Nullable
     public static String toString(@Nullable Object o) {
@@ -22,6 +41,113 @@ public class CoreConversions {
 
         return o.toString();
     }
+
+    public static String safeToString(@Nullable Object o) {
+        if (o == null) {
+            return "";
+        }
+
+        if (o.getClass().isArray()) {
+            return "";
+        }
+
+        if (o instanceof Map) {
+            return "";
+        }
+
+        if (o instanceof Number) {
+            return o.toString();
+        }
+
+        if (o instanceof String) {
+            return o.toString();
+        }
+
+        if (CoreClasses.isPrimitiveOrWrapper(o.getClass())) {
+            return o.toString();
+        }
+
+        String str;
+
+        if ((str = dateToString(o)) != null) {
+            return str;
+        }
+
+        return "";
+    }
+
+    private static String dateToString(Object o) {
+        if (o instanceof Date) {
+            return ((Date) o).toInstant().atOffset(ZoneOffset.UTC).format(DATE_TIME_FORMATTER);
+        }
+
+        if (o instanceof Calendar) {
+            return ((Calendar) o).toInstant().atOffset(ZoneOffset.UTC).format(DATE_TIME_FORMATTER);
+        }
+
+        if (o instanceof Instant) {
+            return ((Instant) o).atOffset(ZoneOffset.UTC).format(DATE_TIME_FORMATTER);
+        }
+
+        if (o instanceof OffsetDateTime) {
+            return ((OffsetDateTime) o).format(DATE_TIME_FORMATTER);
+        }
+
+        if (o instanceof ZonedDateTime) {
+            return ((ZonedDateTime) o).format(DATE_TIME_FORMATTER);
+        }
+
+        if (o instanceof LocalDateTime) {
+            return ((LocalDateTime) o).format(LOCAL_DATE_TIME_FORMATTER);
+        }
+
+        if (o instanceof LocalDate) {
+            return ((LocalDate) o).format(LOCAL_DATE_FORMATTER);
+        }
+
+        if (o instanceof LocalTime) {
+            return ((LocalTime) o).format(LOCAL_TIME_FORMATTER);
+        }
+
+        if (o instanceof TimeZone) {
+            return ((TimeZone) o).getID();
+        }
+
+        if (CoreLibraries.isJodaTimePresent()) {
+            return jodaDateToString(o);
+        }
+
+        return null;
+    }
+
+    private static String jodaDateToString(Object o) {
+        if (o instanceof DateTime) {
+            DateTime dateTime = (DateTime) o;
+            return Instant.ofEpochMilli(dateTime.getMillis()).atZone(dateTime.getZone().toTimeZone().toZoneId()).format(DATE_TIME_FORMATTER);
+        }
+
+        if (o instanceof Instant) {
+            return Instant.ofEpochMilli(((Instant) o).toEpochMilli()).toString();
+        }
+
+        if (o instanceof org.joda.time.LocalDateTime) {
+            org.joda.time.LocalDateTime dt = (org.joda.time.LocalDateTime) o;
+            return LocalDateTime.of(dt.getYear(), dt.getMonthOfYear(), dt.getDayOfMonth(), dt.getHourOfDay(), dt.getMinuteOfHour(), dt.getSecondOfMinute(), dt.getMillisOfSecond() * 1000000).format(LOCAL_DATE_FORMATTER);
+        }
+
+        if (o instanceof org.joda.time.LocalDate) {
+            org.joda.time.LocalDate dt = (org.joda.time.LocalDate) o;
+            return LocalDate.of(dt.getYear(), dt.getMonthOfYear(), dt.getDayOfMonth()).format(LOCAL_DATE_FORMATTER);
+        }
+
+        if (o instanceof org.joda.time.LocalTime) {
+            org.joda.time.LocalTime dt = (org.joda.time.LocalTime) o;
+            return LocalTime.of(dt.getHourOfDay(), dt.getMinuteOfHour(), dt.getSecondOfMinute(), dt.getMillisOfSecond() * 1000000).format(LOCAL_TIME_FORMATTER);
+        }
+
+        return null;
+    }
+
 
     @Nullable
     public static Number toNumber(@Nullable Object o) {
