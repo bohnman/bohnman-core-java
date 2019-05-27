47a863dd682706599a2713c998c87d4d11b8bd8a
Ryan Bohn <rbohn@biohitech.com>
2019-05-27 12:57:18 -0400

    removing json node classes to squiggly

--- src/main/java/com/github/bohnman/core/json/node/CoreJsonNodeFunction.java
+++
@@ -1,6 +0,0 @@
-package com.github.bohnman.core.json.node;
-
-public interface CoreJsonNodeFunction<T> {
-
-    CoreJsonNode<T> apply(CoreJsonNodeVisitorContext context, CoreJsonNode<T> node);
-}
--- src/main/java/com/github/bohnman/core/json/node/CoreJsonNodePredicate.java
+++
@@ -1,7 +0,0 @@
-package com.github.bohnman.core.json.node;
-
-@FunctionalInterface
-public interface CoreJsonNodePredicate<T> {
-
-    boolean test(CoreJsonNodeVisitorContext context, CoreJsonNode<T> node);
-}
